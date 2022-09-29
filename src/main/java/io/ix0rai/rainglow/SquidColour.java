2d79e0dedb33483c7fef4fbba7496bbf71c7d101
ix0rai <ix0rai64@gmail.com>
2022-09-28 19:28:01 -0500

    clean up internals a little

--- src/main/java/io/ix0rai/rainglow/SquidColour.java
+++ src/main/java/io/ix0rai/rainglow/SquidColour.java
@@ -4,93 +4,77 @@ import net.minecraft.util.Identifier;
 
 public enum SquidColour {
     BLUE(
-            "blue",
-            new Identifier("textures/entity/squid/glow_squid.png"),
             new RGB(0.6F, 1.0F, 0.8F),
             new RGB(0.08F, 0.4F, 0.4F),
             new RGB(204, 31, 102)
     ),
     RED(
-            "red",
             new RGB(1.0F, 1.0F, 0.8F),
             new RGB(1.0F, 0.4F, 0.4F),
             new RGB(200, 0, 0)
     ),
     GREEN(
-            "green",
             new RGB(0.6F, 1.0F, 0.8F),
             new RGB(0.08F, 1.0F, 0.4F),
             new RGB(0, 200, 0)
     ),
     PINK(
-            "pink",
             new RGB(0.6F, 0F, 0.5F),
             new RGB(1.0F, 0.1F, 1.0F),
             new RGB(200, 0, 0)
     ),
     YELLOW(
-            "yellow",
             new RGB(1.0F, 1.0F, 0.8F),
             new RGB(1.0F, 1.0F, 0.4F),
             new RGB(200, 0, 0)
     ),
     ORANGE(
-            "orange",
             new RGB(1.0F, 0.2F, 0.3F),
             new RGB(1.0F, 0F, 0.1F),
             new RGB(200, 0, 0)
     ),
     INDIGO(
-            "indigo",
             new RGB(0.6F, 1.0F, 0.8F),
             new RGB(0.08F, 0.4F, 1.0F),
             new RGB(0, 0, 200)
     ),
     PURPLE(
-            "purple",
             new RGB(0.3F, 0F, 0.25F),
             new RGB(0.5F, 0.05F, 0.5F),
             new RGB(200, 0, 100)
     ),
     WHITE(
-            "white",
             new RGB(1.0F, 1.0F, 1.0F),
             new RGB(1.0F, 1.0F, 1.0F),
             new RGB(200, 200, 200)
     ),
     GRAY(
-            "gray",
             new RGB(0.6F, 0.6F, 0.6F),
             new RGB(0.4F, 0.4F, 0.4F),
             new RGB(100, 100, 100)
     ),
     BLACK(
-            "black",
             new RGB(0.0F, 0.0F, 0.0F),
             new RGB(0.0F, 0.0F, 0.0F),
             new RGB(0, 0, 0)
     );
 
-    private final String id;
     private final Identifier texture;
     private final RGB passiveParticleRgb;
     private final RGB altPassiveParticleRgb;
     private final RGB inkRgb;
 
-    SquidColour(String id, RGB passiveParticleRgb, RGB altPassiveParticleRgb, RGB inkRgb) {
-        this(id, new Identifier("textures/entity/squid/" + id + ".png"), passiveParticleRgb, altPassiveParticleRgb, inkRgb);
-    }
-
-    SquidColour(String id, Identifier texture, RGB passiveParticleRgb, RGB altPassiveParticleRgb, RGB inkRgb) {
-        this.id = id;
-        this.texture = texture;
+    SquidColour(RGB passiveParticleRgb, RGB altPassiveParticleRgb, RGB inkRgb) {
+        // blue glow squids use the vanilla glow squid texture, so we can ship fewer textures
+        String textureName = this.getId().equals("blue") ? "glow_squid" : this.getId();
+        this.texture = new Identifier("textures/entity/squid/" + textureName + ".png");
         this.passiveParticleRgb = passiveParticleRgb;
         this.altPassiveParticleRgb = altPassiveParticleRgb;
         this.inkRgb = inkRgb;
     }
 
     public String getId() {
-        return this.id;
+        return this.name().toLowerCase();
     }
 
     public Identifier getTexture() {
@@ -107,6 +91,11 @@ public enum SquidColour {
 
     public RGB getInkRgb() {
         return this.inkRgb;
+    }
+
+    @Override
+    public String toString() {
+        return this.getId();
     }
 
     public static SquidColour get(String id) {
