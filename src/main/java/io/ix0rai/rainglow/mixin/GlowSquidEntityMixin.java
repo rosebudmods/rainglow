a8cdddce532c268a4ed8ebe540b323d0abeadecf
ix0rai <ix0rai64@gmail.com>
2022-10-05 21:06:32 -0500

    clean up squid ink mixin

--- src/main/java/io/ix0rai/rainglow/mixin/GlowSquidEntityMixin.java
+++ src/main/java/io/ix0rai/rainglow/mixin/GlowSquidEntityMixin.java
@@ -3,14 +3,18 @@ package io.ix0rai.rainglow.mixin;
 import io.ix0rai.rainglow.Rainglow;
 import io.ix0rai.rainglow.SquidColour;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.WaterCreatureEntity;
 import net.minecraft.entity.passive.GlowSquidEntity;
 import net.minecraft.entity.passive.SquidEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static io.ix0rai.rainglow.Rainglow.COLOUR;
@@ -57,6 +61,34 @@ public abstract class GlowSquidEntityMixin extends SquidEntity {
             // we add 100 to g to let the mixin know that we want to override the method
             this.world.addParticle(ParticleTypes.GLOW, this.getParticleX(0.6), this.getRandomBodyY(), this.getParticleZ(0.6), Rainglow.getColourIndex(colour) + 100, 0, 0);
             ci.cancel();
+        }
+    }
+
+    @Mixin(SquidEntity.class)
+    public abstract static class SquidEntityMixin extends WaterCreatureEntity {
+        protected SquidEntityMixin(EntityType<? extends WaterCreatureEntity> entityType, World world) {
+            super(entityType, world);
+        }
+
+        /**
+         * @author ix0rai
+         * @reason pass custom colour index to spawnParticles
+         * @implNote we suppress the constant condition warning because intellij doesn't understand mixins
+         * @return value of spawnParticles
+         */
+        @SuppressWarnings("ConstantConditions")
+        @Redirect(method = "squirt", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnParticles(Lnet/minecraft/particle/ParticleEffect;DDDIDDDD)I"))
+        private int spawnParticles(ServerWorld instance, ParticleEffect particle, double x, double y, double z, int count, double deltaX, double deltaY, double deltaZ, double speed) {
+            if (((Object) this) instanceof GlowSquidEntity) {
+                // send in custom colour data
+                String colour = Rainglow.getColour(this.dataTracker, this.random);
+                int index = Rainglow.getColourIndex(colour);
+                // round x to 1 decimal place and append index data to the next two
+                return ((ServerWorld) this.world).spawnParticles(particle, (Math.round(x * 10)) / 10D + index / 1000D, y + 0.5, z, 0, deltaX, deltaY, deltaZ, speed);
+            } else {
+                // normal logic for squid
+                return ((ServerWorld) this.world).spawnParticles(particle, x, y + 0.5, z, 0, deltaX, deltaY, deltaZ, speed);
+            }
         }
     }
 }
