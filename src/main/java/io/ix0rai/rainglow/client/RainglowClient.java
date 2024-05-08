5b8f679586224fc6169fcfe65865c9ab1df33622
ix0rai <ix0rai64@gmail.com>
2024-05-08 16:33:19 -0500

    fix networking so ummm. why is there an autocloseable if i'm not supposed to close it

--- src/main/java/io/ix0rai/rainglow/client/RainglowClient.java
+++ src/main/java/io/ix0rai/rainglow/client/RainglowClient.java
@@ -22,50 +22,48 @@ public class RainglowClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientPlayNetworking.registerGlobalReceiver(RainglowNetworking.ConfigSyncPayload.PACKET_ID, (payload, context) -> {
-            try (MinecraftClient client = context.client()) {
-                client.execute(() -> {
-                    // custom must be set before mode so that if the server sends a custom mode it is set correctly
-                    // otherwise the client's custom would be used
-                    Rainglow.CONFIG.setCustom(payload.customMode());
-                    Rainglow.CONFIG.setMode(RainglowMode.byId(payload.currentMode()));
+            MinecraftClient client = context.client();
+            client.execute(() -> {
+                // custom must be set before mode so that if the server sends a custom mode it is set correctly
+                // otherwise the client's custom would be used
+                Rainglow.CONFIG.setCustom(payload.customMode());
+                Rainglow.CONFIG.setMode(RainglowMode.byId(payload.currentMode()));
 
-                    for (var entry : payload.enabledMobs().entrySet()) {
-                        Rainglow.CONFIG.setEntityEnabled(entry.getKey(), entry.getValue());
-                    }
+                for (var entry : payload.enabledMobs().entrySet()) {
+                    Rainglow.CONFIG.setEntityEnabled(entry.getKey(), entry.getValue());
+                }
 
-                    // lock the config from reloading on resource reload
-                    Rainglow.CONFIG.setEditLocked(true);
+                // lock the config from reloading on resource reload
+                Rainglow.CONFIG.setEditLocked(true);
 
-                    // log
-                    Rainglow.LOGGER.info("received config from server: set mode to " + payload.currentMode() + " and custom colours to " + payload.customMode());
-                });
-            }
+                // log
+                Rainglow.LOGGER.info("received config from server: set mode to " + payload.currentMode() + " and custom colours to " + payload.customMode());
+            });
         });
 
         ClientPlayNetworking.registerGlobalReceiver(RainglowNetworking.ModeSyncPayload.PACKET_ID, (payload, context) -> {
-            try (MinecraftClient client = context.client()) {
-                client.execute(() -> {
-                    List<String> newModeIds = new ArrayList<>();
+            MinecraftClient client = context.client();
+            client.execute(() -> {
+                List<String> newModeIds = new ArrayList<>();
 
-                    // add modes that do not exist on the client to the map
-                    for (RainglowMode mode : payload.modes()) {
-                        if (!mode.existsLocally()) {
-                            newModeIds.add(mode.getId());
-                            RainglowMode.addMode(mode);
-                        }
+                // add modes that do not exist on the client to the map
+                for (RainglowMode mode : payload.modes()) {
+                    if (!mode.existsLocally()) {
+                        newModeIds.add(mode.getId());
+                        RainglowMode.addMode(mode);
                     }
+                }
 
-                    // now that we have modes, we can load the config
-                    if (Rainglow.CONFIG.isUninitialised()) {
-                        Rainglow.CONFIG.reloadFromFile();
-                    }
+                // now that we have modes, we can load the config
+                if (Rainglow.CONFIG.isUninitialised()) {
+                    Rainglow.CONFIG.reloadFromFile();
+                }
 
-                    // log
-                    if (!newModeIds.isEmpty()) {
-                        Rainglow.LOGGER.info("received new modes from server: " + newModeIds);
-                    }
-                });
-            }
+                // log
+                if (!newModeIds.isEmpty()) {
+                    Rainglow.LOGGER.info("received new modes from server: " + newModeIds);
+                }
+            });
         });
 
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) ->
